6abfa7c86edc3ac28c5eeb51311193f76133ca8f
Jimmy <yebdou@gmail.com>
2019-01-17 17:13:57 +0200

    Add []

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -38,13 +38,13 @@ class UserMailer < ApplicationMailer
       @login_url  = 'https://www.omnilint.com/login'
 
       if @repository_access.enable_email_notifications
-        mail(from: "Omnilint <support@omnilint.com>", to: "#{@user.username} <#{@user.email}>", subject: "#{@repository.uuid} - #{@commit_attempt.name}")
+        mail(from: "Omnilint <support@omnilint.com>", to: "#{@user.username} <#{@user.email}>", subject: "[#{@repository.uuid}] - #{@commit_attempt.name}")
       end
 
       if @repository_access.enable_admin_email_notifications
         @repository.repository_accesses.each do |access|
           if access.role == 'admin' && access.user.username != @user.username
-            mail(from: "Omnilint <support@omnilint.com>", to: "#{access.user.username} <#{access.user.email}>", subject: "#{@repository.uuid} - #{@commit_attempt.name}")
+            mail(from: "Omnilint <support@omnilint.com>", to: "#{access.user.username} <#{access.user.email}>", subject: "[#{@repository.uuid}] - #{@commit_attempt.name}")
           end
         end
       end
