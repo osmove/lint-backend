281e2d78f1243ab4e1007c9f22921f28d1a0e4e8
Jimmy <yebdou@gmail.com>
2019-01-27 18:25:21 +0200

    Email when message created

---
+++ app/mailers/message_mailer.rb
@@ -0,0 +1,19 @@
+class MessageMailer < ApplicationMailer
+
+  # default from: 'notifications@example.com'
+
+  def message_created_email(message)
+    @message = message
+    @site_url  = 'https://www.omnilint.com'
+    @login_url  = 'https://www.omnilint.com/login'
+
+    # Send email to client
+    # mail(from: "Omnilint <support@omnilint.com>", to: "<#{message.to_email}>", subject: "New message: '#{message.subject}'")
+    mail(from: "Omnilint <support@omnilint.com>", to: "jimmydouieb@gmail.com", subject: "New message: '#{message.subject}'")
+
+    # Send copy
+    # mail(from: "Omnilint <support@omnilint.com>", to: "Jimmy Douieb <jimmydouieb@gmail.com>", subject: "Repository '#{@repository.user.username}/#{@repository.name}' was created successfully.")
+
+  end
+
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,19 @@
 class Message < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :lead, optional: true
-  
+
   self.inheritance_column = :_type_disabled
+
+  after_create :send_created_email
+  def send_created_email
+    puts "%%%%%%"
+    puts "FROM"
+    puts self.email
+    puts "TO"
+    puts self.to_email
+    # if self.to_email.present?
+      MessageMailer.message_created_email(self).deliver_now
+    # end
+  end
 
 end
