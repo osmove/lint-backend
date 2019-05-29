29e3545cda11c477086fe18f32c9b31dc28dcfae
Jimmy Douieb <jimmydouieb@gmail.com>
2019-05-29 15:41:37 +0300

    Add /callback

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,8 @@ end
 
 
 Devise.setup do |config|
-  config.omniauth :github, 'aae25e7b1428bbec9be7', '33c19ebca07374476393ee83d6d2cecc18c1fa2d', scope: 'read:user, public_repo, read:org', :redirect_uri => 'http://localhost:3000/users/auth/github'
+  config.omniauth :github, 'aae25e7b1428bbec9be7', '33c19ebca07374476393ee83d6d2cecc18c1fa2d', scope: 'read:user, public_repo, read:org', :redirect_uri => 'http://localhost:3000/users/auth/github/callback'
+  # config.omniauth :github, 'aae25e7b1428bbec9be7', '33c19ebca07374476393ee83d6d2cecc18c1fa2d', scope: 'read:user, public_repo, read:org', :redirect_uri => 'http://localhost:3000/users/auth/github'
   # config.omniauth :github, 'aae25e7b1428bbec9be7', '33c19ebca07374476393ee83d6d2cecc18c1fa2d', scope: 'user, repo, read:org, admin:repo_hook, repo_deployment', :redirect_uri => 'http://localhost:3000/users/auth/github'
 end
 
